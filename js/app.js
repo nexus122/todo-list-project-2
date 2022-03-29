dd427fda6878c3e96a60519429849395bd5349b3
nexus122 <juan-pablo122@hotmail.es>
2022-03-29 13:30:01 +0200

    Ejercicio de TODO resuelto.

--- js/app.js
+++ js/app.js
@@ -1,115 +1,95 @@
-//add an eventListener to the from
-const form = document.querySelector('#itemForm'); // select form
-const itemInput = document.querySelector('#itemInput'); // select input box from form
-const itemList = document.querySelector('.item-list');
-const feedback = document.querySelector('.feedback');
-const clearButton = document.querySelector('#clear-list');
-
-let todoItems = [];
-
-const handleItem = function(itemName){
-
-    const items = itemList.querySelectorAll('.item');
- 
-    items.forEach(function(item){
-        
-        if(item.querySelector('.item-name').textContent === itemName){
-            //complete event listener
-            item.querySelector('.complete-item').addEventListener('click', function(){
-                item.querySelector('.item-name').classList.toggle('completed');
-                this.classList.toggle('visibility');
-            });
-            //edit event listener
-            item.querySelector('.edit-item').addEventListener('click', function(){
-                itemInput.value = itemName;
-                itemList.removeChild(item);
-
-                todoItems = todoItems.filter(function(item){
-                    return item !== itemName;
-                });
-            });
-            // delete event listener
-            item.querySelector('.delete-item').addEventListener('click', function(){
-                debugger;
-                itemList.removeChild(item);
-
-                todoItems = todoItems.filter(function(item){
-                    return item !== itemName;
-                });
-
-                showFeedback('item delete', 'success');
-            })
-        }
-    })
+let estado = {
+    items: {},
 }
 
-const removeItem = function(item){
-    console.log(item);
-    const removeIndex = (todoItems.indexOf(item));
-    console.log(removeIndex);
-    todoItems.splice(removeIndex, 1);
-}
-
-const getList = function(todoItems){
-    itemList.innerHTML = '';
-
-        todoItems.forEach(function(item){
-            itemList.insertAdjacentHTML('beforeend', `<div class="item my-3"><h5 class="item-name text-capitalize">${item}</h5><div class="item-icons"><a href="#" class="complete-item mx-2 item-icon"><i class="far fa-check-circle"></i></a><a href="#" class="edit-item mx-2 item-icon"><i class="far fa-edit"></i></a><a href="#" class="delete-item item-icon"><i class="far fa-times-circle"></i></a></div></div>` );
-
-            handleItem(item);
-        });
-}
-
-const getLocalStorage = function(){
-
-    const todoStorage = localStorage.getItem('todoItems');
-    if (todoStorage === 'undefined' || todoStorage === null){
-        todoItems = [];
-    } else {
-        todoItems = JSON.parse(todoStorage);
-        getList(todoItems);
-    }
-}
-
-const setLocalStorage = function(todoItems){
-    localStorage.setItem('todoItems', JSON.stringify(todoItems));
-}
-
-// get local storage from page
-getLocalStorage();
-
-//add an item to the List, including to local storage
-form.addEventListener('submit', function(e){ 
+// Añadir un item.
+$('#itemForm').on('submit', function (e) {
+    // No permitimos que se envie el formulario
     e.preventDefault();
-    const itemName = itemInput.value;
-    
-    if (itemName.length === 0){
-        feedback.innerHTML = 'Please Enter Valid Value';
-        feedback.classList.add('showItem', 'alert-danger');
-        setTimeout(
-            function(){
-                feedback.classList.remove('showItem');
-                }, 3000);
-    } else {
-        todoItems.push(itemName);
-        setLocalStorage(todoItems);
-        getList(todoItems);
-        //add event listeners to icons;
-        //handleItem(itemName);
+    // Obtenemos el valor del input
+    let value = $('#itemInput').val();
+
+    // Controlamos si lo que nos envia el usuario es vacio.
+    if (value.trim() == "") {
+        $('#itemInput').val('');
+        return;
     }
-    
-    itemInput.value = '';
 
-    });
+    // Borramos el input
+    $('#itemInput').val('');
+    newRow(value);
+});
 
-    //clear all items from the list
-clearButton.addEventListener('click', function(){
-    todoItems = [];
-    localStorage.clear();
-    getList(todoItems);
+/* EVENTOS */
+// Accion de completar la tarea
+$(document).on("click", ".complete-item", function () {
+    console.log('Se ha completado la tarea.');
+
+    let padre = $(this).parent().parent().find('h5');
+    let termino = $(this).parent().parent().find('h5').text();
+
+    if (padre.hasClass('completed')) {
+        estado.items[termino].completed = false;        
+        padre.removeClass('completed');
+    } else {
+        padre.addClass('completed');
+        estado.items[termino].completed = true;
+    }
+
+});
+
+// Accion editar el contenido
+$(document).on("click", ".edit-item", function () {
+    console.log('Editar la tarea.');
+
+    // Tomamos la referencia del padre y del texto.
+    let padre = $(this).parent().parent();
+    let termino = $(this).parent().parent().find('h5').text();
+
+    // Llevamos el nombre de la tarjeta al input para "editarlo"
+    $('#itemInput').val(termino);
+
+    // borramos el item en nuestra variable global.
+    delete estado.items[termino];
+
+    // Eliminamos el elemento.
+    padre.remove();
+});
+
+// Accion de borrar el item.
+$(document).on("click", ".delete-item", function () {
+    console.log('Se ha borrado la tarea.');
+
+    // Tomamos la referencia del padre y del texto.
+    let padre = $(this).parent().parent();
+    let termino = $(this).parent().parent().find('h5').text();
+
+    // Eliminamos la propiedad del objeto para no tener la fila guardada en estado.    
+    delete estado.items[termino];
+
+    // Eliminamos al hijo.
+    padre.remove();
+});
+
+$('#clear-list').click(function () {
+    // Vaciamos todos los items.
+    estado.items = {};
+    $(".item-container").html('');
 })
 
+function newRow(itemName) {
 
+    let template = `
+    <div class="item my-3">
+        <h5 class="item-name text-capitalize">${itemName}</h5>
+        <div class="item-icons">
+            <a href="#" class="complete-item mx-2 item-icon"><i class="far fa-check-circle"></i></a>
+            <a href="#" class="edit-item mx-2 item-icon"><i class="far fa-edit"></i></a>
+            <a href="#" class="delete-item item-icon"><i class="far fa-times-circle"></i></a>
+        </div>
+    </div>`
 
-  
+    estado.items[itemName] = { name: itemName, html: template, completed: false };
 
+    $(".item-container").append(template);
+}
